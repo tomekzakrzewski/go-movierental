d8ebdd6d1cbc9af238c83d86fa055fbea66674d0
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2024-02-17 07:26:29 +0100

    add auth handler test, test authenticate success and failure

---
+++ api/auth_handler_test.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/tomekzakrzewski/go-movierental/db/fixtures"
+)
+
+func TestAuthenticateFailure(t *testing.T) {
+	tdb := setup(t)
+	defer tdb.teardown(t)
+	fixtures.AddUser(tdb.Store, "james", "foo", false)
+
+	app := fiber.New()
+	authHandler := NewAuthHandler(tdb.User)
+	app.Post("/auth", authHandler.HandleAuthenticate)
+
+	params := AuthParams{
+		Email:    "james@foo.com",
+		Password: "supersecurepasswordnotcorrect",
+	}
+	b, _ := json.Marshal(params)
+	req := httptest.NewRequest("POST", "/auth", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Fatalf("expected http status of 400 but got %d", resp.StatusCode)
+	}
+	var genResp genericResp
+	if err := json.NewDecoder(resp.Body).Decode(&genResp); err != nil {
+		t.Fatal(err)
+	}
+	if genResp.Type != "error" {
+		t.Fatalf("expected gen response type to be error but got %s", genResp.Type)
+	}
+	if genResp.Msg != "invalid credentials" {
+		t.Fatalf("expected gen response msg to be <invalid credentials> but got %s", genResp.Msg)
+	}
+}
+
+func TestAuthenticateSuccess(t *testing.T) {
+	tdb := setup(t)
+	defer tdb.teardown(t)
+	user := fixtures.AddUser(tdb.Store, "tomek", "test", false)
+
+	app := fiber.New()
+	authHandler := NewAuthHandler(tdb.User)
+	app.Post("/auth", authHandler.HandleAuthenticate)
+
+	params := AuthParams{
+		Email:    user.Email,
+		Password: "test123",
+	}
+	b, _ := json.Marshal(params)
+	req := httptest.NewRequest("POST", "/auth", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	var authResp AuthResponse
+	json.NewDecoder(resp.Body).Decode(&authResp)
+	if authResp.User.ID != user.ID {
+		t.Fatalf("expected user id %s but got %s", user.ID, authResp.User.ID)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Fatalf("expected http status of 400 but got %d", resp.StatusCode)
+	}
+}
